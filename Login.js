c08249a54143de87c5a62415a7c55a1e79fca1b1
Sahanaj <sahanaj961@gmail.com>
2023-05-13 10:22:00 +0530

    login from

---
+++ Login.js
@@ -0,0 +1,12 @@
+const loginForm = document.querySelector('.login-form');
+const registerForm = document.querySelector('.register-form');
+
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	// Add code to handle login form submission
+});
+
+registerForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	// Add code to handle register form submission
+});
