d8b7a55714783e356d5dd068c14ce8158ac22950
Trisha <mona2001.trisha@gmail.com>
2023-05-14 12:30:42 +0530

    vid:11

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,9 @@ let searchForm = document.querySelector('.search-form');
  document.querySelector('#search-btn').onclick = () =>
  {
     searchForm.classList.toggle('active');
+   
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
  }
 
  let shoppingCart= document.querySelector('.shopping-cart');
@@ -10,4 +13,28 @@ let searchForm = document.querySelector('.search-form');
  document.querySelector('#cart-btn').onclick = () =>
  {
    shoppingCart.classList.toggle('active');
+
+   searchForm.classList.remove('active');
+   navbar.classList.remove('active');
  }
+
+
+ let navbar= document.querySelector('.navbar');
+
+ document.querySelector('#menu-btn').onclick = () =>
+ {
+   navbar.classList.toggle('active');
+
+   searchForm.classList.remove('active');
+   shoppingCart.classList.remove('active');
+
+   
+ }
+
+ window.onscroll = () =>
+ {
+   searchForm.classList.remove('active');
+   shoppingCart.classList.remove('active');
+   navbar.classList.remove('active');
+ }
+
