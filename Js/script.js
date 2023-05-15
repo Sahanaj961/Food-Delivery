9ac63e151359495cb103ebff7331671a5b600518
Piu154 <120847583+Piu154@users.noreply.github.com>
2023-05-15 14:34:22 +0530

    g

--- Js/script.js
+++ Js/script.js
@@ -47,4 +47,28 @@ let searchForm = document.querySelector('.search-form');
   
  }
 
+ var swiper = new Swiper(".product-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      
+    },
+    768: {
+      slidesPerView: 1,
+      
+    },
+    1024: {
+      slidesPerView: 3,
+      
+    },
+  },
+});
+
 
